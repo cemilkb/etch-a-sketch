67cd8c2eeec5558a9a9805de206edd9c8c2734d1
cemilkb <cemilkoybasi98hotmail.com>
2023-08-22 12:55:52 +0300

    css düzenlemeleri dış görünüş son

---
+++ script.js
@@ -0,0 +1,17 @@
+// let ekle = document.querySelector(".container")
+
+// for(let i = 1; i <= 16; i++  ){
+//     let div = document.createElement("div")
+//     div.setAttribute("class", "row")
+
+//     for(let j = 1; j <= 16; j++){
+//         let div2 = document.createElement("div")
+//         div2.setAttribute("class", "column")
+//         div2.addEventListener("click", ()=>{
+//             div2.style.backgroundColor = "red"
+//         })
+//         div.appendChild(div2)
+//     }
+
+//     ekle.appendChild(div)
+// }
