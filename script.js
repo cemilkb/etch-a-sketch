fe2beede46a86cce967111640444237f201cc5dd
cemilkb <cemilkoybasi98hotmail.com>
2023-08-22 14:13:25 +0300

    tablonun pixelizasyonunu yaptım

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
-// let ekle = document.querySelector(".container")
+let ekle = document.querySelector(".sketch")
+let pixel = document.getElementById("pixel")
+let pixelX = document.getElementById("pixelX")
 
-// for(let i = 1; i <= 16; i++  ){
-//     let div = document.createElement("div")
-//     div.setAttribute("class", "row")
+// Functions
 
-//     for(let j = 1; j <= 16; j++){
-//         let div2 = document.createElement("div")
-//         div2.setAttribute("class", "column")
-//         div2.addEventListener("click", ()=>{
-//             div2.style.backgroundColor = "red"
-//         })
-//         div.appendChild(div2)
-//     }
+function pixelTable(){
+    let isOkay = false
+    
+    ekle.innerHTML = ""
+    pixelX.textContent = `${pixel.value}x${pixel.value}`
 
-//     ekle.appendChild(div)
-// }
+    for (let i = 1; i <= pixel.value; i++) {
+
+        let div = document.createElement("div")
+
+        div.setAttribute("class", "row")
+
+        for (let j = 1; j <= pixel.value; j++) {
+
+            let div2 = document.createElement("div")
+
+            div2.setAttribute("class", "column")
+
+            div2.addEventListener("mousedown", () => {
+                isOkay = true
+                div2.style.backgroundColor = "red"
+            })
+
+            div2.addEventListener("mousemove", () => {
+                if (isOkay) {
+                    div2.style.backgroundColor = "red"
+                }
+            })
+
+            div2.addEventListener("mouseup", () => {
+                isOkay = false
+            })
+
+            div.appendChild(div2)
+        }
+
+        ekle.appendChild(div)
+    }
+}
+
+// For Beginning
+
+pixelTable()
+
+// Eent Listener
+pixel.addEventListener("change", pixelTable)
+
+
+
+
+
+
